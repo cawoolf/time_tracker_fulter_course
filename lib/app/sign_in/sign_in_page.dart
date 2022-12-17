904e6a5d817b5e7508f04b0e2171fc09117a97a9
Cameron Woolf <cameron.woolf@beatconnect.com>
2022-12-17 11:30:19 -0600

    progressss

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -19,31 +19,55 @@ class SignInPage extends StatelessWidget {
   // the _methodName is convention for making the method private
   // only accessible at the file level
   Widget _buildContent() {
-    return Container(
-      color: Colors.yellow,
+    return Padding(
+      //Container with Padding with no background
+      // color: Colors.yellow,
+      padding: const EdgeInsets.all(16), // 16 What? Dp?
       child: Column(
+        // Columns used for creating vertical layout
+        //Basically vertical alignment
+        mainAxisAlignment: MainAxisAlignment.center,
+        // The crossAxisAlignment property determines how Row and Column can position their children on their cross axes.
+        // Essentially Horizontal Alignment
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-              color: Colors.orange,
-              child: const SizedBox(
-                //width: 100, Don't need width field if Alignment set to stretch
-                height: 100,
-              )),
-          Container(
-              color: Colors.red,
-              child: const SizedBox(
-                //width: 100, Don't need width field if Alignment set to stretch
-                height: 100,
-              )),
-          Container(
-              color: Colors.purple,
-              child: const SizedBox(
-                //width: 100, Don't need width field if Alignment set to stretch
-                height: 100,
-              )),
+          _signInText(),
+          _spaceBetweenWidgets(),
+          _googleSignInButton(),
+          _spaceBetweenWidgets(),
         ],
       ), // The child of a Container can be any Widget in Flutter
     );
+  }
+
+  Text _signInText() {
+    return const Text(
+      "Sign in",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  SizedBox _spaceBetweenWidgets() {
+    return const SizedBox(height: 8.0);
+  }
+
+  ElevatedButton _googleSignInButton() {
+    return ElevatedButton(
+        onPressed: () {
+          _signInWithGoogle();
+        },
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+        child: const Text(
+          "Sign in with Google",
+          style: TextStyle(color: Colors.black),
+        ));
+  }
+
+  void _signInWithGoogle() {
+    print('Google Sign in clicked: Authenticating with Google');
   }
 }
