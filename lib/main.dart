27c58dee01aea5745f1a7015a7f36c85242eb751
cawoolf <rayadevinfo@gmail.com>
2023-04-10 14:08:19 -0600

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // Main entry point for the entire Flutter app
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //await stops code execution until the Future has been returned.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp()); // Takes a root/home Widget for the app
 }
 
