da08049ba3e2b2543836998fde6827cdd30a666d
cawoolf <rayadevinfo@gmail.com>
2023-06-06 18:43:24 -0600

    210, 211 Platform aware dialogs

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
+import '../common_widgets/show_alert_dialog.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key,required this.auth, /*required this.onSignOut*/}) : super(key: key);
+  const HomePage({Key? key, required this.auth, /*required this.onSignOut*/
+  }) : super(key: key);
 
   // final void Function() onSignOut;
   final AuthBase auth;
@@ -17,6 +20,20 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  // Why is this async?
+  /*
+  Ohh we have to await for the User to interact with the Alert and press a button, which returns a boolean
+   */
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await showAlertDialog(context, title: 'Logout',
+        content: 'Are you sure you want to logout?',
+        cancelActionText: 'Cancel',
+        defaultActionText: 'Logout');
+    if(didRequestSignOut == true) {
+      _signOut();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +41,7 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         actions: <Widget>[
           TextButton(
-              onPressed: _signOut,
+              onPressed: () => _confirmSignOut(context),
               child: Text(
                 'Logout',
                 style: TextStyle(fontSize: 18.0, color: Colors.white),
--- lib/common_widgets/show_alert_dialog.dart
+++ lib/common_widgets/show_alert_dialog.dart
@@ -3,21 +3,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-Future showAlertDialog(BuildContext context,
-    {required String title,
-    required String content,
-    required String defaultActionText}) {
+/* The { } are used to define named parameters. When you use this method in other classes,
+you will see the name of the variable you are passing an argument to. */
+Future showAlertDialog(
+  BuildContext context, {
+  required String title,
+  required String content,
+  required String defaultActionText,
+  String? cancelActionText,
+}) {
   // Returns a regularAlertDialog if the Platform is web or any other OS beside iOS
   if (kIsWeb || !Platform.isIOS) {
     print('Inside Platform if');
     return showDialog(
         context: context,
+        barrierDismissible: false, // Makes it so you cannot dismiss the dialog by clicking outside it.
         builder: (context) => AlertDialog(
                 title: Text(title),
                 content: Text(content),
                 actions: <Widget>[
+                  if (cancelActionText !=
+                      null) // Example of collection if in dart. It's inside the builder.
+                    ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(false), // Does not pop the last displayed page off the stack, and causes the method to return false
+                        child: Text(cancelActionText)),
                   ElevatedButton(
-                      onPressed: () => Navigator.of(context).pop(true),
+                      onPressed: () => Navigator.of(context).pop(true), // Pops the last displayed page off the stack, and causes the method to return true
                       child: Text(defaultActionText))
                 ]));
   }
@@ -27,7 +38,12 @@ Future showAlertDialog(BuildContext context,
               title: Text(title),
               content: Text(content),
               actions: <Widget>[
-                ElevatedButton(
+                if (cancelActionText !=
+                    null) // Example of collection if in dart. It's inside the builder.
+                  CupertinoButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: Text(cancelActionText)),
+               CupertinoButton(
                     onPressed: () => Navigator.of(context).pop(true),
                     child: Text(defaultActionText))
               ]));
