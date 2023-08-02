98bc72bb9f3ec19c9030b3e37822326f48a2866d
cawoolf <rayadevinfo@gmail.com>
2023-08-01 21:12:00 -0600

    281 Section 17: Defining a strongly typed model class

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../app/home/models/job.dart';
+
 abstract class Database {
 
-  Future<void> createJob(Map<String,dynamic> jobData);
+  Future<void> createJob(Job job);
 
 }
 
@@ -11,9 +13,9 @@ class FirestoreDatabase implements Database {
   final String uid;
 
   @override
-  Future<void> createJob(Map<String, dynamic> jobData) async {
+  Future<void> createJob(Job job) async {
     final path ='users/$uid/jobs/job_abc'; // Test path to write to FireStore
     final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(jobData);
+    await documentReference.set(job.toMap());
   }
 }
