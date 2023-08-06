1c1ec5bcb05abad95472fcea796baf3536011302
cawoolf <rayadevinfo@gmail.com>
2023-08-06 16:08:02 -0600

    290 Section 17: Adding a factory constructor to our Model Class

--- lib/app/home/models/job.dart
+++ lib/app/home/models/job.dart
@@ -3,6 +3,20 @@ class Job {
   final String name;
   final int ratePerHour;
 
+  /* factory methods keep all data conversion inside the model class itself
+  Best practice. factory constructor doesn't always return a new instance
+  can return null
+
+  fromMap() and toMap() are common practices*/
+  factory Job.fromMap(Map<String, dynamic> data) {
+     final String name = data['name'];
+     final int ratePerHour = data['ratePerHour'];
+     return Job (
+         name: name,
+     ratePerHour: ratePerHour);
+
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'name' : name,
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,21 +4,21 @@ import 'package:time_tracker_flutter_course/services/api_path.dart';
 import '../app/home/models/job.dart';
 
 abstract class Database {
-
   Future<void> createJob(Job job);
-  Stream<List<Job?>> jobsStream();
-  void readJobs();
 
+  Stream<List<Job?>> jobsStream();
+
+  void readJobs();
 }
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({required this.uid});
+
   final String uid;
 
   @override
-  Future<void> createJob(Job job) => _setData(
-      path: APIPath.job(uid, 'job_abc2'),
-      data: job.toMap());
+  Future<void> createJob(Job job) =>
+      _setData(path: APIPath.job(uid, 'job_abc2'), data: job.toMap());
 
   @override
   Stream<List<Job?>> jobsStream() {
@@ -27,7 +27,8 @@ class FirestoreDatabase implements Database {
 
     // snapshots() is a reference that returns a Stream<QuerySnapshot>
     // a snapshot is an instance of FireStore collection at any given time.
-    final snapshots = reference.snapshots(); //Returns a stream of individual snapshots
+    final snapshots =
+        reference.snapshots(); //Returns a stream of individual snapshots
     // The snapshot represents a Firestore Collection. In this case a collection of Jobs.
     snapshots.listen((snapshots) {
       for (var snapshot in snapshots.docs) {
@@ -35,17 +36,12 @@ class FirestoreDatabase implements Database {
       }
     });
 
-    return snapshots.map((snapshot) => snapshot.docs.map(
-            (snapshot){
-              final data = snapshot.data();
-              return data != null ? Job(
-                name: data['name'],
-                ratePerHour: data['ratePerHour'],
-              ) : null;
-            }).toList()  // Common mistake is to convert the Iterable into a List .map() returns an Iterable
-    );
+    return snapshots.map((snapshot) => snapshot.docs
+        .map((snapshot) => Job.fromMap(snapshot
+                .data())
+            )
+        .toList()); // Common mistake is to convert the Iterable into a List .map() returns an Iterable
   }
-
 
   // Debugging/Troubleshooting method
   @override
@@ -55,7 +51,8 @@ class FirestoreDatabase implements Database {
 
     // snapshots() is a reference that returns a Stream<QuerySnapshot>
     // a snapshot is an instance of FireStore collection at any given time.
-    final snapshots = reference.snapshots(); //Returns a stream of individual snapshots
+    final snapshots =
+        reference.snapshots(); //Returns a stream of individual snapshots
     // The snapshot represents a Firestore Collection. In this case a collection of Jobs.
     snapshots.listen((snapshots) {
       for (var snapshot in snapshots.docs) {
@@ -65,11 +62,10 @@ class FirestoreDatabase implements Database {
   }
 
   // Defines a single entry point to all FireStore writes. Best practice. Good for debuging
-  Future<void> _setData({required String path, required Map<String, dynamic> data}) async {
+  Future<void> _setData(
+      {required String path, required Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path: $data');
     await reference.set(data);
-
   }
-
 }
