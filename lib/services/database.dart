2f9e33abb44133c9125f6b3cda82ed07ef771fd1
cawoolf <rayadevinfo@gmail.com>
2023-08-08 20:41:03 -0600

    293 Section 17: Adding a Firestore Service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,13 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_tracker_flutter_course/services/api_path.dart';
+import 'package:time_tracker_flutter_course/services/firestore_service.dart';
 
 import '../app/home/models/job.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
-
   Stream<List<Job?>> jobsStream();
-
 }
 
 class FirestoreDatabase implements Database {
@@ -15,75 +14,19 @@ class FirestoreDatabase implements Database {
 
   final String uid;
 
+  final _service = FirestoreService.instance; //Singleton for the service class.
+
   @override
   Future<void> createJob(Job job) =>
-      _setData(path: APIPath.job(uid, 'job_abc2'), data: job.toMap());
+      _service.setData(path: APIPath.job(uid, 'job_abc2'), data: job.toMap());
 
-
-  // Defines a single entry point to all FireStore writes. Best practice. Good for debuging
-  Future<void> _setData(
-      {required String path, required Map<String, dynamic> data}) async {
-    final reference = FirebaseFirestore.instance.doc(path);
-    print('$path: $data');
-    await reference.set(data);
-  }
-
+  /* I don't understand where the data variable is coming from.. Ohhh data isn't being assinged here,
+  this is just a parameter for the anonymous function being used inside _collectionStream
+  _collectionStream assignees the data
+*/
   @override
-  Stream<List<Job?>> jobsStream() => _collectionStream(
-      path: APIPath.jobs(uid),
-      builder: (data) => Job.fromMap(data));
+  Stream<List<Job?>> jobsStream() =>
+      _service.collectionStream(
+          path: APIPath.jobs(uid), builder: (data) => Job.fromMap(data));
 
-
-  // Generics for handling different types of Collections. Not just Jobs
-  Stream<List<T>> _collectionStream<T>({
-    required String path,
-    required T Function(Map<String, dynamic> data) builder,
-  }) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-
-    return snapshots.map((snapshot) =>
-        snapshot.docs.map((snapshot) => builder(snapshot.data())).toList());
-  }
-
-}
-
-// Notes methods. Not for use
-Stream<List<Job?>> notesJobsStream() {
-  String uid = 'test';
-  final path = APIPath.jobs(uid);
-  final reference = FirebaseFirestore.instance.collection(path);
-
-  // snapshots() is a reference that returns a Stream<QuerySnapshot>
-  // a snapshot is an instance of FireStore collection at any given time.
-  final snapshots =
-  reference.snapshots(); //Returns a stream of individual snapshots
-  // The snapshot represents a Firestore Collection. In this case a collection of Jobs.
-  snapshots.listen((snapshots) {
-    for (var snapshot in snapshots.docs) {
-      print(snapshot.data()); //This data is a list of all Jobs
-    }
-  });
-
-  return snapshots.map((snapshot) => snapshot.docs
-      .map((snapshot) => Job.fromMap(snapshot.data()))
-      .toList()); // Common mistake is to convert the Iterable into a List .map() returns an Iterable
-}
-
-// Debugging/Troubleshooting method
-void readJobs() {
-  String uid = 'test';
-  final path = APIPath.jobs(uid);
-  final reference = FirebaseFirestore.instance.collection(path);
-
-  // snapshots() is a reference that returns a Stream<QuerySnapshot>
-  // a snapshot is an instance of FireStore collection at any given time.
-  final snapshots =
-  reference.snapshots(); //Returns a stream of individual snapshots
-  // The snapshot represents a Firestore Collection. In this case a collection of Jobs.
-  snapshots.listen((snapshots) {
-    for (var snapshot in snapshots.docs) {
-      print(snapshot.data()); //This data is a list of all Jobs
-    }
-  });
 }
---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../app/home/models/job.dart';
+import 'api_path.dart';
+
+// Class for seperating out the Firestore functions from the database class.
+class FirestoreService {
+
+  //Singleton patern for Dart. We only want one instance of this service class.
+  FirestoreService._(); // Private constructor
+  static final instance = FirestoreService._();
+
+  // Defines a single entry point to all FireStore writes. Best practice. Good for debuging
+  Future<void> setData(
+      {required String path, required Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('$path: $data');
+    await reference.set(data);
+  }
+
+  // Generics for handling different types of Collections. Not just Jobs
+  Stream<List<T>> collectionStream<T>({
+    required String path,
+    required T Function(Map<String, dynamic> data) builder,
+  }) {
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+
+    return snapshots.map((snapshot) =>
+        snapshot.docs.map((snapshot) => builder(snapshot.data())).toList()); //data is assigned here
+  }
+}
+
+
+// Notes methods. Not for use. collectionStream is a refactor of this.
+Stream<List<Job?>> notesJobsStream() {
+  String uid = 'test';
+  final path = APIPath.jobs(uid);
+  final reference = FirebaseFirestore.instance.collection(path);
+
+  // snapshots() is a reference that returns a Stream<QuerySnapshot>
+  // a snapshot is an instance of FireStore collection at any given time.
+  final snapshots =
+  reference.snapshots(); //Returns a stream of individual snapshots
+  // The snapshot represents a Firestore Collection. In this case a collection of Jobs.
+  snapshots.listen((snapshots) {
+    for (var snapshot in snapshots.docs) {
+      print(snapshot.data()); //This data is a list of all Jobs
+    }
+  });
+
+  return snapshots.map((snapshot) => snapshot.docs
+      .map((snapshot) => Job.fromMap(snapshot.data()))
+      .toList()); // Common mistake is to convert the Iterable into a List .map() returns an Iterable
+}
