bb46080d3b910916450a6f4ac97d4b0a4f04b6aa
cawoolf <rayadevinfo@gmail.com>
2023-08-12 20:58:55 -0600

    300 Section 18: Validating and saving form data - 3:18

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -17,12 +17,39 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if(form!.validate()) {
+      form.save();
+      return true;
+    }
+      return false;
+  }
+
+  void _submit() {
+    // TODO: Validate and save form
+    if(_validateAndSaveForm()) {
+      print('form saved');
+    }
+    // TODO: Submit data to Firestore
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
         title: const Text('New Job'),
+        actions: <Widget>[
+          ElevatedButton(
+              onPressed: _submit,
+              child: const Text('Save',
+                  style: TextStyle(fontSize: 18, color: Colors.white)))
+        ],
+
         // backgroundColor: Colors.grey[200],
       ),
       body: _buildContent(),
@@ -31,20 +58,17 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildContent() {
     return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Card(
-          child: Padding(
+        child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: _buildForm()
-          )
-        )
-      )
-    );
+            child: Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: _buildForm()))));
   }
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -53,17 +77,16 @@ class _AddJobPageState extends State<AddJobPage> {
   }
 
   List<Widget> _buildFormChildren() {
-      return [
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'Job name'),
-        ),
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'Rate per hour'),
-          keyboardType: const TextInputType.numberWithOptions(
-            signed: false,
-            decimal: false
-          ),
-        )
-      ];
+    return [
+      TextFormField(
+        decoration: const InputDecoration(labelText: 'Job name'),
+      ),
+      TextFormField(
+        decoration: const InputDecoration(labelText: 'Rate per hour'),
+        keyboardType: const TextInputType.numberWithOptions(
+            signed: false, decimal: false),
+      )
+    ];
   }
+
 }
