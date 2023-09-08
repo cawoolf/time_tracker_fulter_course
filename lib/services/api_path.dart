557ec24ce25babf474c3bcac5031fb7a2735c1d9
cawoolf <rayadevinfo@gmail.com>
2023-09-07 20:36:18 -0600

    322 Section 20: Getting ready to add new files to the project

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -2,7 +2,9 @@
 
 // Common API class for keeping track of FireStore Paths. Best practice
 class APIPath {
-  static String job(String uid, String jobId) => '/users/$uid/jobs/$jobId'; //Returns a single target job
+  static String job(String uid, String jobId) => 'users/$uid/jobs/$jobId'; //Returns a single target job
   static String jobs(String uid) => 'users/$uid/jobs';  //Returns all jobs for a User
+  static String entry(String uid, String entryId) => 'users/$uid/entries/$entryId';
+  static String entries(String uid) => 'users/$uid/entries';
 
 }
