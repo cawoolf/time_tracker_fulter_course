c557973fb6306f0c15186c95af499adbaa91d734
cawoolf <rayadevinfo@gmail.com>
2023-09-27 18:51:04 -0600

    339 Section 20: Adding a CupertinoTabView

--- lib/app/home/cupertino_home_scaffold.dart
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
@@ -5,14 +6,33 @@ import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 // Knows how to show the bottom navigation on screen
 // Allows us to swap out the UI easily if we want
 class CupertinoHomeScaffold extends StatelessWidget {
-  const CupertinoHomeScaffold({Key? key,
-    required this.currentTab,
-    required this.onSelectTab}) : super(key: key);
+  const CupertinoHomeScaffold(
+      {Key? key, required this.currentTab, required this.onSelectTab})
+      : super(key: key);
 
   final TabItem currentTab;
   final ValueChanged<TabItem> onSelectTab; //Callback
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          _buildItem(TabItem.jobs),
+          _buildItem(TabItem.entries),
+          _buildItem(TabItem.account)],
+        onTap: (index) => onSelectTab(TabItem.values[index]),
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return const CupertinoTabView();
+      },
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    final itemData = TabItemData.allTabs[tabItem];
+    final color = currentTab == tabItem ? Colors.indigo : Colors.grey;
+    return BottomNavigationBarItem(
+        icon: Icon(itemData?.icon, color: color),
+        label: itemData?.label);
   }
 }
