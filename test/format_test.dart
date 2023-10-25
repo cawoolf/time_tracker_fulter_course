596316f1c9721e5cef509996d04c65a980a8cd6c
cawoolf <rayadevinfo@gmail.com>
2023-10-25 17:33:59 -0600

    375 Section 24: The setUp method and testing date formatting with locales

--- test/format_test.dart
+++ test/format_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:intl/intl.dart';
 import 'package:time_tracker_flutter_course/app/home/job_entries/format.dart';
 
 void main() {
-  group('hours',() {
+  group('hours', () {
     test('positive', () {
       expect(Format.hours(10), '10h');
     });
@@ -17,6 +20,26 @@ void main() {
 
     test('decimal', () {
       expect(Format.hours(4.5), '4.5h');
+    });
+  });
+
+  group('data - GB Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en-GB'; // Important to localize tests if needed
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2023-10-25',() {
+      expect(Format.date(DateTime(2023,10,25)), '25 Oct 2023');
+    });
+  });
+
+  group('data - US Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en-US'; // Important to localize tests if needed
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('2023-10-25',() {
+      expect(Format.date(DateTime(2023,10,25)), 'Oct 25, 2023');
     });
   });
 }
