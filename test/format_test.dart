ae95ecb65df7d8c7d6f91693a1e4733577514ab5
cawoolf <rayadevinfo@gmail.com>
2023-10-25 21:31:39 -0600

    377 Section 24: Completing the formatting tests

--- test/format_test.dart
+++ test/format_test.dart
@@ -28,8 +28,8 @@ void main() {
       Intl.defaultLocale = 'en-GB'; // Important to localize tests if needed
       await initializeDateFormatting(Intl.defaultLocale);
     });
-    test('2023-10-25',() {
-      expect(Format.date(DateTime(2023,10,25)), '25 Oct 2023');
+    test('2023-10-25', () {
+      expect(Format.date(DateTime(2023, 10, 25)), '25 Oct 2023');
     });
   });
 
@@ -38,8 +38,33 @@ void main() {
       Intl.defaultLocale = 'en-US'; // Important to localize tests if needed
       await initializeDateFormatting(Intl.defaultLocale);
     });
-    test('2023-10-25',() {
-      expect(Format.date(DateTime(2023,10,25)), 'Oct 25, 2023');
+    test('2023-10-25', () {
+      expect(Format.date(DateTime(2023, 10, 25)), 'Oct 25, 2023');
+    });
+  });
+
+  group('dayOfWeek - GB Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en-GB'; // Important to localize tests if needed
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Wednesday', () {
+      expect(Format.dayOfWeek(DateTime(2023, 10, 25)), 'Wed');
+    });
+  });
+
+  group('currency - US Locale', () {
+    setUp(() => Intl.defaultLocale = 'en-US');
+    test('positive', () {
+      expect(Format.currency(10), '\$10');
+    });
+
+    test('zero', () {
+      expect(Format.currency(0), '');
+    });
+
+    test('negative', () {
+      expect(Format.currency(-5), '-\$5');
     });
   });
 }
