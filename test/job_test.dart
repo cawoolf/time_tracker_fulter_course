1a027bf147e3f969f6d2a00c3f63ec7acb0018e4
cawoolf <rayadevinfo@gmail.com>
2023-10-29 16:42:40 -0600

    378 Section 24: Testing model classes - toMap()

--- test/job_test.dart
+++ test/job_test.dart
@@ -4,10 +4,7 @@ import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 void main() {
   group('fromMap',() {
     // test('null data', () {
-    //   final job = Job.fromMap({
-    //     'name': null,
-    //      'ratePerHour':null
-    //   }, 'abc');
+     // final job = Job.fromMap(null, 'abc'); //fromMap() is null safe
     //   expect(job, null);
     // });
 
@@ -19,6 +16,16 @@ void main() {
       // expect(job, Job(name: 'Blogging', ratePerHour: 10, id: 'abc'));
       expect(job.name, 'Blogging');
       expect(job.ratePerHour, 10);
+    });
+  });
+
+  group('toMap',() {
+
+    test('job to map',()
+    {
+      final job = Job(name: 'test', ratePerHour: 10, id: 'abc');
+      expect(job.toMap()['name'], 'test');
+      expect(job.toMap()['ratePerHour'],10);
     });
   });
 }
