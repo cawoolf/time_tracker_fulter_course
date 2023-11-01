968cd80bc6abee1b012670a377302d6d940b32d5
cawoolf <rayadevinfo@gmail.com>
2023-10-31 21:23:21 -0600

    383 Section 24: Testing widget callbacks

--- test/custom_raised_button_test.dart
+++ test/custom_raised_button_test.dart
@@ -3,13 +3,24 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
 void main() {
-  testWidgets('',(WidgetTester tester ) async {
+  testWidgets('onPressed callback',(WidgetTester tester ) async {
+    var pressed = false;
+
     await tester.pumpWidget(MaterialApp(home:CustomRaisedButton(color: Colors.black12, //Widgets should always have an ancestor MaterialApp()
-    onPressed: () {  },
+    onPressed: () => pressed = true,
     child: const Text('tap me'),)));
+
     final button = find.byType(ElevatedButton);
     expect(button, findsOneWidget); //Finds a Widget of the type declared by button inside the hierarchy of the Widget under test.
     expect(find.byType(FloatingActionButton), findsNothing); // Expects not the find a Widget of the declared type
     expect(find.text('tap me'), findsOneWidget); //Expects to find a Widget with the text 'tap me'
+
+    // Can test the functionality of call backs using variables and tester
+    // tester has many gestures that can be tested. Must use await
+    await tester.tap(button);  // Calls the onPressed callback.
+    expect(pressed, true);
+
+
+
   });
 }
